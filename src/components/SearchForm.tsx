2e4dfbaf182f7d5a3099263ba343650fb31b80d8
Sulit Jr, Victor <victor.sulit.jr@accenture.com>
2024-12-24 15:01:51 +0800

    add search page

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,6 +1,8 @@
+"use client";
+import { useState } from "react";
 import { Search } from "lucide-react";
-
 import { Label } from "@/components/ui/label";
+import { useRouter } from "next/navigation";
 import {
   SidebarGroup,
   SidebarGroupContent,
@@ -8,8 +10,18 @@ import {
 } from "@/components/ui/sidebar";
 
 export function SearchForm({ ...props }: React.ComponentProps<"form">) {
+  const [query, setQuery] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (query.trim()) {
+      router.push(`/search?q=${query}`);
+    }
+  };
+
   return (
-    <form {...props}>
+    <form onSubmit={handleSubmit} {...props}>
       <SidebarGroup className="py-0">
         <SidebarGroupContent className="relative">
           <Label htmlFor="search" className="sr-only">
@@ -17,7 +29,9 @@ export function SearchForm({ ...props }: React.ComponentProps<"form">) {
           </Label>
           <SidebarInput
             id="search"
-            placeholder="Search the anime..."
+            placeholder="Search anime..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
             className="pl-8"
           />
           <Search className="pointer-events-none absolute left-2 top-1/2 size-4 -translate-y-1/2 select-none opacity-50" />
