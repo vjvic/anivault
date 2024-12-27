80f06c688aa8befe6aaab6eefca927f6d00cb7b4
Sulit Jr, Victor <victor.sulit.jr@accenture.com>
2024-12-27 12:45:28 +0800

    spacing adjustment

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const PageContainer = ({ children }: { children: React.ReactNode }) => {
-  return <div className="py-6 container mx-auto">{children}</div>;
+  return <div className="py-6 container mx-auto lg:px-0 px-4">{children}</div>;
 };
 
 export default PageContainer;
--- src/components/ShowcaseCharacters.tsx
+++ src/components/ShowcaseCharacters.tsx
@@ -12,7 +12,7 @@ const ShowcaseCharacters = ({
   characters: AnimeResponse<Characters>;
 }) => {
   return (
-    <div className="container mx-auto mt-10">
+    <div className="container mx-auto mt-10 lg:px-0 px-4">
       <div className="flex justify-between items-center mb-3 border-l-4 border-primary pl-2">
         <h3 className="text-3xl font-semibold">{text}</h3>
         <Link href="/top/characters" className="text-primary">
